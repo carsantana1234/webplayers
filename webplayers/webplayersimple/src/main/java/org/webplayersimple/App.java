652ca5266f41db2afba849515c242ec29a1cd4f9
Carlos <carsantana1234@gmail.com>
2013-11-24 21:40:58 +0100

    New Software

---
+++ webplayers/webplayersimple/src/main/java/org/webplayersimple/App.java
@@ -0,0 +1,13 @@
+package org.webplayersimple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
