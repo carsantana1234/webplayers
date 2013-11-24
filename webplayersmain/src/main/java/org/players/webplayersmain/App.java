06b3d33746104ee627c52e65ca863c8f5fdb3165
Carlos <carsantana1234@gmail.com>
2013-11-24 22:34:38 +0100

    new web

---
+++ webplayersmain/src/main/java/org/players/webplayersmain/App.java
@@ -0,0 +1,13 @@
+package org.players.webplayersmain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
